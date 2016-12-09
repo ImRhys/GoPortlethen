82bbc5da2ec2b3a0dd7f50ab77c3687f885823c8
HDocwra <hdocwra@gmail.com>
2016-12-09 19:23:49 +0000

    Removed the constants from db.class.php

--- resources/Config/config.class.php
+++ resources/Config/config.class.php
@@ -4,43 +4,44 @@ namespace Config;
 
 
 class Config {
-    private $config;
+  private $config;
 
-    public function __construct() {
-        $this->config = $this->setConfig();
-    }
+  public function __construct() {
+    $this->config = $this->setConfig();
+  }
 
-    private function setConfig() {
-        return [
-            "sitename" => "GoPortlethen"
-        ];
-    }
+  private function setConfig() {
+    return [
+      "sitename" => "GoPortlethen",
+      "dbname" => "test"
+    ];
+  }
 
-    /**
-     * Get a value from the config array with passed key
-     * @param $key array key
-     * @return mixed array value for key
-     */
-    public function get($key) {
-        if (isset($this->config[$key])) {
-            return $this->config[$key];
-        }
+  /**
+   * Get a value from the config array with passed key
+   * @param $key array key
+   * @return mixed array value for key
+   */
+  public function get($key) {
+    if (isset($this->config[$key])) {
+      return $this->config[$key];
     }
+  }
 
-    /**
-     * Add a key and value in the config array
-     * @param $key array key
-     * @param $value array value
-     */
-    public function add($key, $value) {
-        $this->config[$key] = $value;
-    }
+  /**
+   * Add a key and value in the config array
+   * @param $key array key
+   * @param $value array value
+   */
+  public function add($key, $value) {
+    $this->config[$key] = $value;
+  }
 
-    /**
-     * Returns the entire config array
-     * @return array config aray
-     */
-    public function getall() {
-        return $this->config;
-    }
+  /**
+   * Returns the entire config array
+   * @return array config aray
+   */
+  public function getall() {
+    return $this->config;
+  }
 }
