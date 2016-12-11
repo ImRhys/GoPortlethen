7f44be30314a4f0bee7eb3aca13003f3673af599
HDocwra <hdocwra@gmail.com>
2016-12-11 16:59:55 +0000

    Not the best place for that

--- global.php
+++ global.php
@@ -2,5 +2,11 @@
 
 require("resources/classloader.php");
 
+if (\Config\Config::get("displayerror")) {
+  ini_set('display_errors', 1);
+  ini_set('display_startup_errors', 1);
+  error_reporting(E_ALL);
+}
+
 $db = new \Database\db();
 $page = new \Page\page();
