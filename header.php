01e1e8a5bf23f1e7c1edafe0422c91035c84e00e
HDocwra <hdocwra@gmail.com>
2016-12-12 04:58:42 +0000

    header formatted

--- header.php
+++ header.php
@@ -1,116 +1,114 @@
-<!DOCTYPE HTML>
-
 <!-- Navigation / START -->
 <nav class="navbar navbar-default">
-    <div class="container">
+  <div class="container">
 
-        <div class="navbar-header">
-            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
-                <span class="sr-only">Show menu</span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-            </button>
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar"
+              aria-expanded="false" aria-controls="navbar">
+        <span class="sr-only">Show menu</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
 
-            <a class="navbar-brand" id="logo" href="index.php">
-                <span class="highlight-text">G</span>o<span class="highlight-text">P</span>ortlethen
-            </a>
-        </div>
-
-
-        <div id="navbar" class="navbar-collapse collapse">
-
-            <ul class="nav navbar-nav">
-
-                <li class="drop active">
-                    <a href="index.php">
-                        Home
-                    </a>
-                    <div class="dropdown">
-                        <ul>
-                            <li>
-                                <a href="#">About</a>
-                            </li>
-                        </ul>
-                        <ul>
-                            <li>
-                                <a href="#">Contact</a>
-                            </li>
-                        </ul>
-                    </div>
-                </li>
-
-                <li class="drop">
-                    <a href="#">
-                        Health
-                    </a>
-                    <div class="dropdown">
-                        <ul>
-                            <li>
-                                <a href="#">News</a>
-                            </li>
-                        </ul>
-                        <ul>
-                            <li>
-                                <a href="#">Events</a>
-                            </li>
-                        </ul>
-                    </div>
-                </li>
-
-                <li class="drop">
-                    <a href="clubs.php">
-                        Clubs
-                    </a>
-                    <div class="dropdown">
-                        <ul>
-                            <li>
-                                <a href="#">Create a Club Page</a>
-                            </li>
-                        </ul>
-                        <ul>
-                            <li>
-                                <a href="#">Search Club Pages</a>
-                            </li>
-                        </ul>
-                    </div>
-                </li>
-
-                <li class="drop">
-                    <a href="#">
-                        Maps
-                    </a>
-                    <div class="dropdown">
-                        <ul>
-                            <li>
-                                <a href="#">Viewpoints</a>
-                            </li>
-                        </ul>
-                        <ul>
-                            <li>
-                                <a href="#">Routes</a>
-                            </li>
-                        </ul>
-                    </div>
-                </li>
-
-                <li class="drop">
-                    <a href="login.php">
-                        Login
-                    </a>
-                    <div class="dropdown">
-                        <ul>
-                            <li>
-                                <a href="register.php">Register</a>
-                            </li>
-                        </ul>
-                    </div>
-                </li>
-
-            </ul>
-
-        </div>
-
+      <a class="navbar-brand" id="logo" href="index.php">
+        <span class="highlight-text">G</span>o<span class="highlight-text">P</span>ortlethen
+      </a>
     </div>
 
+
+    <div id="navbar" class="navbar-collapse collapse">
+      <ul class="nav navbar-nav">
+        <li class="drop active">
+          <a href="index.php">
+            Home
+          </a>
+
+          <div class="dropdown">
+            <ul>
+              <li>
+                <a href="#">About</a>
+              </li>
+            </ul>
+            <ul>
+              <li>
+                <a href="#">Contact</a>
+              </li>
+            </ul>
+          </div>
+        </li>
+
+        <li class="drop">
+          <a href="#">
+            Health
+          </a>
+
+          <div class="dropdown">
+            <ul>
+              <li>
+                <a href="#">News</a>
+              </li>
+            </ul>
+            <ul>
+              <li>
+                <a href="#">Events</a>
+              </li>
+            </ul>
+          </div>
+        </li>
+
+        <li class="drop">
+          <a href="clubs.php">
+            Clubs
+          </a>
+
+          <div class="dropdown">
+            <ul>
+              <li>
+                <a href="#">Create a Club Page</a>
+              </li>
+            </ul>
+            <ul>
+              <li>
+                <a href="#">Search Club Pages</a>
+              </li>
+            </ul>
+          </div>
+        </li>
+
+        <li class="drop">
+          <a href="#">
+            Maps
+          </a>
+
+          <div class="dropdown">
+            <ul>
+              <li>
+                <a href="#">Viewpoints</a>
+              </li>
+            </ul>
+            <ul>
+              <li>
+                <a href="#">Routes</a>
+              </li>
+            </ul>
+          </div>
+        </li>
+
+        <li class="drop">
+          <a href="login.php">
+            Login
+          </a>
+
+          <div class="dropdown">
+            <ul>
+              <li>
+                <a href="register.php">Register</a>
+              </li>
+            </ul>
+          </div>
+        </li>
+      </ul>
+    </div>
+  </div>
 </nav><!-- Navigation / END -->
