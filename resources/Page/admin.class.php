af5be2c2aebb4327201be9a5e22fb336ac9541ec
HDocwra <hdocwra@gmail.com>
2016-12-12 04:33:27 +0000

    Added the start of the member bay of bayness

--- resources/Page/admin.class.php
+++ resources/Page/admin.class.php
@@ -5,4 +5,14 @@ namespace Page;
 
 class admin extends page {
 
+  /**
+   * Check login
+   */
+  public function checkLogin() {
+    //if we're not logged in redirect us
+    if (empty($_SESSION['user'])) {
+      header("Location: login.php?loginfailed=1");
+      die("Access denied.");
+    }
+  }
 }
