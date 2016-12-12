15a8fe368981971088ab0804371d14485bef4d89
HDocwra <hdocwra@gmail.com>
2016-12-12 04:08:46 +0000

    register.php merged with registerfront.php

--- register.php
+++ register.php
@@ -21,6 +21,12 @@ if (!empty($_POST)) {
   if (empty($_POST['password'])) {
     die("Please enter a password.");
   }
+  if (empty($_POST['password2'])) {
+    die("Please repeat your password.");
+  }
+  if ($_POST['password'] !== $_POST['password2']) {
+    die("Passwords don't match.");
+  }
   if (!filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
     die("Invalid E-Mail Address");
   }
@@ -81,59 +87,119 @@ if (!empty($_POST)) {
   die("Registered");
 }
 
-$page->addCSS("bootstrap.css");
+$page->setPageTitle("Login");
+$page->setPageDescription("Login Page");
+
+$page->addHeaderHTML('<meta charset="utf-8"/>');
+$page->addHeaderHTML('<meta name="viewport" content="width=device-width, initial-scale=1"/>');
+$page->setPageTitle(\Config\Config::get("sitename") . ' - Register');
+$page->setPageDescription($page->getPageTitle());
+
+
+//Fonts
+$page->addCSS("https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css", false);
+
+//Style
+$page->addCSS("https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css", false);
+$page->addCSS("main.css");
+$page->addCSS("orange.css");
 $page->addCSS("extra.css");
 
 $page->renderHeader();
 $page->echoHeader();
 ?>
-  <form class="form-horizontal" action="register.php" method="post">
-    <fieldset>
-      <legend>Register</legend>
-      <div class='form-group'>
-        <label class='col-md-4 control-label' for='name'>Full Name</label>
 
-        <div class='col-md-5'>
-          <input id='fullname' name='fullname' type='text' placeholder='e.g John Smith' class='form-control input-md'
-                 required=''>
-        </div>
-      </div>
-      <div class="form-group">
-        <label class="col-md-4 control-label" for="username">Username</label>
+  <!-- Navigation include -->
+<?php include 'header.php'; ?>
 
-        <div class="col-md-4">
-          <input id="username" name="username" type="text" placeholder="e.g jsmith1" class="form-control input-md"
-                 required="">
-        </div>
-      </div>
-      <div class="form-group">
-        <label class="col-md-4 control-label" for="email">Email</label>
+  <div id="content">
+    <section id="register">
+      <div class="container">
+        <div class="row">
+          <div
+            class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3 col-lg-4 col-lg-offset-4 md-margin-top">
+            <div class="page-subheader text-center">
+              <h1>Register</h1>
+              <hr/>
+              <p>Register as a member to create and contribute to clubs and maps!</p>
+            </div>
 
-        <div class="col-md-4">
-          <input id="email" name="email" type="text" placeholder="e.g johnsmith@example.com"
-                 class="form-control input-md" required="">
-        </div>
-      </div>
-      <div class="form-group">
-        <label class="col-md-4 control-label" for="password">Password</label>
+            <form action="#" method="post">
 
-        <div class="col-md-4">
-          <input id="password" name="password" type="password" placeholder="" class="form-control input-md"
-                 required="">
-        </div>
-      </div>
-      <div class='form-group'>
-        <label class='col-md-4 control-label' for='submit'></label>
+              <div class="form-group">
+                <div class="controls">
+                  <input name="fullname" type="text" placeholder="Full Name" class="form-control input"/>
+                </div>
+              </div>
 
-        <div class='col-md-8'>
-          <button id='submit' name='submit' class='btn btn-primary'>Submit</button>
+              <div class="form-group">
+                <div class="controls">
+                  <input name="username" type="text" placeholder="Username" class="form-control input"/>
+                </div>
+              </div>
+
+              <div class="form-group">
+                <div class="controls">
+                  <input name="email" type="text" placeholder="Email" class="form-control input"/>
+                </div>
+              </div>
+
+              <div class="form-group">
+                <div class="controls">
+                  <input name="password" type="password" placeholder="Password" class="form-control input"/>
+                </div>
+              </div>
+
+              <div class="form-group">
+                <div class="controls">
+                  <input name="password2" type="password" placeholder="Repeat password" class="form-control input"/>
+                </div>
+              </div>
+
+              <div class="form-group">
+                <div class="controls">
+                  <div class="checkbox">
+                    <input name="checkbox" id="checkbox1" value="checkbox1" checked="" type="checkbox">
+                    <label for="checkbox1">
+                      I agree and have read <a href="#">terms and conditions</a>.
+                    </label>
+                  </div>
+                </div>
+              </div>
+
+              <div class="form-group">
+                <button name="submit" class="btn btn-block btn-primary">Sign up</button>
+              </div>
+            </form>
+          </div>
         </div>
+        <!-- Row / END -->
       </div>
-    </fieldset>
-  </form>
+      <!-- Container / END -->
+    </section>
+  </div><!-- Content / END -->
+
+
+  <!-- Footer / START -->
+  <footer class="footer">
+    <div class="container">
+      <span class="copyright">
+        Copyright <?= \Config\Config::get("copyrightyear") ?>. All rights served.
+      </span>
+
+      <span class="links">
+        <a href="#">Terms of service</a>
+        <a href="#">Privacy policy</a>
+      </span>
+    </div>
+  </footer><!-- Footer / END -->
+
 <?php
-$page->addFooterJS("jquery-3.1.1.min.js");
-$page->addFooterJS("bootstrap.min.js");
+$page->addFooterJSifIE9("https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js", false);
+$page->addFooterJSifIE9("https://oss.maxcdn.com/libs/respond.js/1.3.0/respond.min.js", false);
+$page->addFooterJS("https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js", false);
+$page->addFooterJS("main.js");
+$page->addFooterJS("https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js", false);
 $page->renderFooter();
 $page->echoFooter();
 ?>
