8d43e4b6d3a09abaa9e2d28a80f1bc15e9f58c79
HDocwra <hdocwra@gmail.com>
2016-12-12 18:45:09 +0000

    Added user profile edit ability

--- member.php
+++ member.php
@@ -44,40 +44,12 @@ $p = $_GET['p'];
     </ul>
   </div>
 
-<?php if ($p === "profile") { ?>
-  <div class="content">
-    <div class="container pushdown">
-      <sectiond id="profile">
-        <div class="panel panel-default">
-          <div class="panel-heading clearfix">
-            <h3 class="panel-title pull-left">Profile Summary</h3>
-            <a href="" class="pull-right">
-              <button class="btn btn-default btn-xs">Edit</button>
-            </a>
-          </div>
-          <table class="table">
-            <tr>
-              <td class="col-md-2 text-right"><b>Username</b></td>
-              <td><?= $_SESSION['user']['userName'] ?></td>
-            </tr>
-            <tr>
-              <td class="col-md-2 text-right"><b>Full Name</b></td>
-              <td><?= $_SESSION['user']['displayName'] ?></td>
-            </tr>
-            <tr>
-              <td class="col-md-2 text-right"><b>Email Address</b></td>
-              <td><?= $_SESSION['user']['emailAddress'] ?></td>
-            </tr>
-            <tr>
-              <td class="col-md-2 text-right"><b>Access Level</b></td>
-              <td><?= $_SESSION['user']['accessName'] ?></td>
-            </tr>
-          </table>
-        </div>
-      </sectiond>
-    </div>
-  </div>
-<?php } ?>
+
+
+<?php
+include 'elements/memberprofile.php';
+include 'elements/membereprofile.php';
+?>
 
   <!-- Footer / START -->
   <footer class="footer">
---
+++ resources/Database/Queries/user.class.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Database\Queries;
+
+
+class user extends \Database\query {
+
+  function __construct(\Database\db $db) {
+    $this->setDBHandle($db);
+  }
+
+  /**
+   * @Override
+   * Execute the SQL Query with fetch
+   */
+  public function runQuery() {
+    $stmt = $this->db->prepare($this->query);
+    $stmt->execute($this->parameters);
+    $this->result = $stmt->rowCount();
+    $this->stmt = $stmt;
+  }
+
+  /**
+   * @Override
+   *
+   * (Unsure if the superfunction will be called instead of our custom one)
+   *
+   * Perform a quick one line query
+   * @param string $query Query string
+   * @param array $parameters Parameter Array
+   * @return array result
+   */
+  public function quickQuery($query, $parameters) {
+    $this->setQuery($query);
+    $this->setParameters($parameters);
+    $this->runQuery();
+    return $this->getResult();
+  }
+
+}
